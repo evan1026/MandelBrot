d31189dd92347982be331d0933ec14581109a656
Evan Allan <evanallan1026@gmail.com>
2013-08-11 10:02:41 -0400

    Making it work and such

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
-#include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
 #include "Logger.hpp"
+#include "main.hpp"
+#include <sstream>
+
+#define WIDTH 500
+#define HEIGHT 300
 
 using namespace std;
 
+bool running = true;
+
 int main(int argc, char *argv[]){
     
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::Window window(sf::VideoMode(WIDTH, HEIGHT), "My window");
     window.setVerticalSyncEnabled(true);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, WIDTH, HEIGHT, 0, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glDisable(GL_DEPTH_TEST);
+
     Logger::log("Initialized window.");
     
-    bool running = true;
+    int pixX = 0;
+    int pixY = 1;
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
     while (running){
         
         sf::Event event;
@@ -24,7 +41,10 @@ int main(int argc, char *argv[]){
             }
         }
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        render(pixX, pixY, window);
+        pixX = (pixX + 1) % WIDTH;
+        if (pixX == 0) pixY++;
+        if (pixY == HEIGHT + 1) running = false;
 
         window.display();
     }
@@ -36,3 +56,57 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
+void render(int pixX, int pixY, sf::Window& window){
+    glBegin(GL_POINTS);
+    //Logger::log(intToString(pixX) + " " + intToString(pixY));
+
+    sf::Vector2u size = window.getSize();
+    double x0 = (double) pixX / (double) size.x * 3.5 - 2.5;
+    double y0 = (double) pixY / (double) size.y * 2.0 - 1.0;
+    //Logger::log(floatToString(x0) + " " + floatToString(y0));
+    int iteration = 0;
+    int max_iteration = 1000;
+
+    double x = 0;
+    double y = 0;
+
+    while ((x*x) + (y*y) < 4 && iteration < max_iteration){
+        double xtemp = x*x - y*y + x0;
+        y = 2 * x * y + y0;
+        
+        x = xtemp;
+        
+        iteration++;
+        //Logger::log(intToString(x) + " " + intToString(y) + " " + intToString(iteration));
+    }
+
+    if (iteration >= max_iteration){
+        glColor3f(0, 0, 0);
+    }
+    else{
+        long temp = iteration * (16777216 / max_iteration);
+        float red = 0, green = 0, blue = 0;
+        red = temp / 65025;
+        temp -= red * 65025;
+        green = temp / 255;
+        temp -= green * 255;
+        blue = temp;
+        //Logger::log(intToString(pixX) + " " + intToString(pixY) + " " + intToString(red) + " " + intToString(green) + " " + intToString(blue));
+        glColor3f(red / 255, green / 255, blue / 255);
+    }
+
+    glVertex2i(pixX, pixY);
+    glEnd();
+}
+
+string intToString(long long int val){
+    stringstream ss;
+    ss << val;
+    return  ss.str();
+}
+
+string floatToString(float val){
+    stringstream ss;
+    ss << val;
+    return ss.str();
+}
---
+++ src/main.hpp
@@ -0,0 +1,8 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+void render(int pixX, int pixY, sf::Window& window);
+std::string intToString(long long int val);
+std::string floatToString(float val);
+
+#endif
